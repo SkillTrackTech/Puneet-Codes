c43ca84908d70be599e5de4bf4c7007023779384
puneetgoyal006 <puneetgoyal86390@gmail.com>
2025-06-05 23:24:29 +0530

    Table of n numbers

---
+++ i-o.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int a ;
+    cout<<"Enter your number:";
+    cin>>a; 
+
+    cout<<"\n"<<a*1;
+    cout<<"\n"<<a*2;
+    cout<<"\n"<<a*3;
+    cout<<"\n"<<a*4;
+    cout<<"\n"<<a*5;
+    cout<<"\n"<<a*6;
+    cout<<"\n"<<a*7;
+    cout<<"\n"<<a*8;
+    cout<<"\n"<<a*9;
+    cout<<"\n"<<a*10;
+
+
+    return 0 ;
+}
