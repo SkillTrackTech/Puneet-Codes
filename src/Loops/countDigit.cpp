fe15496453c9ddefe7d870bd470c11e29652d1ce
puneetgoyal006 <puneetgoyal86390@gmail.com>
2025-06-15 12:02:57 +0530

    Created seperated folders for if-else & loops

---
+++ src/Loops/countDigit.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+int  n;
+    cout<< "Enter a positive number :";
+    cin>>n;
+
+    int count = 0;
+
+    while( n > 0 )
+    {
+	n = n/10;
+	count++;
+    }
+
+    cout<<"Number of digits are : "<<count<<endl;
+
+    return 0;
+}
--- src/Loops/loops4.cpp
+++ src/Loops/loops4.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() 
+
 {
     int rows = 5;
 
