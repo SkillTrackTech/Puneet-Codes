2212b240cae705576ebaac649549413ce0a1d720
puneetgoyal006 <puneetgoyal86390@gmail.com>
2025-06-16 21:44:43 +0530

    New codes are added

---
+++ src/Loops/prime.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i, j;
+    cin >> n;
+
+    for (i = 2; i <= n; i++) {
+        int isPrime = 1;
+        for (j = 2; j < i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime == 1) cout << i << " ";
+    }
+    return 0;
+}
